9fcf448ecf718f337578db1ea6395dde26184966
zcollevechio <zcollevechio@wildfigdata.com>
2020-03-23 11:11:36 -0400

    change colors

--- R/palettes.R
+++ R/palettes.R
@@ -1,18 +1,18 @@
 
 scale_fill_fig_d <- function(){
-  scale_fill_brewer(palette = "Set1")
+  scale_fill_brewer(palette = "Dark2")
 }
 
 scale_color_fig_d <- function(){
-  scale_color_brewer(palette = "Set1")
+  scale_color_brewer(palette = "Dark2")
 }
 
 scale_fill_fig_c <- function(){
-  scale_fill_viridis_c(option = "inferno")
+  scale_fill_viridis_c(option = "magma")
 }
 
 scale_color_fig_c <- function(){
-  scale_color_viridis_c(option = "inferno", end = .95)
+  scale_color_viridis_c(option = "magma")
 }
 
 
