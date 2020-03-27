420d82d25f708b7ba74838a5c65e656b19f7c1b7
zcollevechio <zcollevechio@wildfigdata.com>
2020-03-27 09:42:06 -0400

    add direction arg

--- R/palettes.R
+++ R/palettes.R
@@ -1,18 +1,18 @@
 
-scale_fill_fig_d <- function(){
-  scale_fill_brewer(palette = "Dark2")
+scale_fill_fig_d <- function(direction = 1){
+  scale_fill_brewer(palette = "Dark2", direction = direction)
 }
 
-scale_color_fig_d <- function(){
-  scale_color_brewer(palette = "Dark2")
+scale_color_fig_d <- function(direction = 1){
+  scale_color_brewer(palette = "Dark2", direction = direction)
 }
 
-scale_fill_fig_c <- function(){
-  scale_fill_viridis_c(option = "magma")
+scale_fill_fig_c <- function(direction = 1){
+  scale_fill_viridis_c(option = "magma", direction = direction)
 }
 
-scale_color_fig_c <- function(){
-  scale_color_viridis_c(option = "magma")
+scale_color_fig_c <- function(direction = 1){
+  scale_color_viridis_c(option = "magma", direction = direction)
 }
 
 
